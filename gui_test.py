aae3c36e3e299fde184eeef120bea754847dac20
Mayonezyck <71305052+Mayonezyck@users.noreply.github.com>
2022-10-28 17:05:20 -0400

    Delete gui_test.py

--- gui_test.py
+++
@@ -1,6 +0,0 @@
-from guizero import App, Text, TextBox, Box, PushButton
-app = App(title="Log in", height=250, width=400)
-open_message = Text(app, text="Welcome!", size=40, )
-user_id = Text(app, text="User ID: ",align="left", grid=[3,3])
-id_box = TextBox(app,text="Please enter your ID number", width=25, align="left", grid=[3,4])
-app.display()
