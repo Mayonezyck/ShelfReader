efbe0ce2678858accd005ff9e2e12627f5468834
Xinyi Xu <xxu@trincoll.edu>
2023-02-10 19:58:51 -0500

    Update readfile.py

--- readfile.py
+++ readfile.py
@@ -1,6 +1,7 @@
-import pandas
+import pandas as pd
 
 
-df = pandas.read_csv('example.csv', index_col='barcode')
+#df = pandas.read_csv('example.csv', names=['call number','x','title','barcode','xx','xxx','xxxx'])
 
-print(df)
+d = pd.DataFrame(pd.read_excel('shelflist.xlsx'))
+print(d)
