139afd34eeb71f72004bfac814b2a88a9e6b5116
Xinyi Xu <xxu@trincoll.edu>
2023-02-17 14:18:58 -0500

    Update login_UI.py

--- login_UI.py
+++ login_UI.py
@@ -5,22 +5,37 @@ import csv
 import Student
 
 class login_UI:
+
+	path = 'studentInfo.csv'
 	
 	def addStaff(id):
+		f_write=open(path, 'w')
+		csvwriter = csv.writer(f_write)
+		csvwriter.writerow(id) 
+		f_write.close()
+
+
 		print('here')#
 
 	def checkExist(id):
-		with open("studentInfo.csv", 'r+') as file:
-			csvreader = csv.reader(file)
-			for row in csvreader:
-				print(row)
-				if id == row:
-					return Student.student(id)
-				else:
-					login_UI.addStaff(id)
-		return None
-
-	
+		# with open(path, 'r+') as file:
+		# 	csvreader = csv.reader(file)
+		# 	for row in csvreader:
+		# 		print(row)
+		# 		if id == row:
+		# 			return Student.student(id)
+		# 		else:
+		# 			login_UI.addStaff(id)
+		f_read=open(path, 'r')
+		csvreader = csv.reader(file)
+		for row in csvreader:
+			print(row)
+			if id == row:
+				return Student.student(id)
+			else:
+				login_UI.addStaff(id)
+				return Student.student(id)
+		f_read.close()
 
 
 login_UI.checkExist(1)
