9efda5c5009d1a5005582a13d36c5378f225421b
Xinyi Xu <xxu@trincoll.edu>
2023-03-03 20:39:09 -0500

    nearly finish login_UI.py

--- login_UI.py
+++ login_UI.py
@@ -6,6 +6,7 @@ import Student
 
 filename = 'studentInfo.csv'
 loginWindow = App(title="Log in")
+instruct = Text(loginWindow, text="Please enter your ID number")
 Id_number = ' '
 
 def addStaff(id):
@@ -29,15 +30,20 @@ def checkExist(id):
 def login_UI(student):
 	Text(loginWindow, text="\nWelcome!\n", size=40)
 	Text(loginWindow, text="User ID: ",align="left")
-	UserIdBox = TextBox(loginWindow,text="Please enter your ID number", width=25, align="left")
+	UserIdBox = TextBox(loginWindow,instruct.disable(), width=25, align="left")
 	PushButton(loginWindow, text = 'clear', command = clearTextBox, args = [UserIdBox], align = "left")
-	Id_number = str(UserInput.value)
+	
+	Id_number = str(UserIdBox.value)
 	if len(Id_number) != 7:
 		loginWindow.error('!!!', 'ID number must be 7 digit!')
 	else:
 		login_UI(Id_number)
 	print(Id_number)
-    PushButton(loginWindow, text = 'OK', align = "bottom")
-    # # PushButton(loginWindow, text = 'OK', command = GUInterface_listGenerate, args = [loginWindow], align = "bottom")
+    
+	PushButton(loginWindow, text = 'OK', align = "bottom")
+    # PushButton(loginWindow, text = 'OK', command = GUInterface_listGenerate, args = [loginWindow], align = "bottom")
     # loginWindow.set_full_screen()
-    loginWindow.display()
+	loginWindow.display()
+
+def clearTextBox(textBox):
+    textBox.clear()
