4a04ba9384cb3054771d0a0005e050c6bd4228ba
Xinyi Xu <xxu@trincoll.edu>
2023-03-04 00:32:48 -0500

    create task_UI.py

---
+++ task_UI.py
@@ -0,0 +1,30 @@
+# confirm the date and the booklist waiting for check
+
+from guizero import *
+from datetime import *
+import os
+# import readfile
+import login_UI
+
+# get today's date in textual month, day and year	
+today = date.today().strftime("%B %d, %Y") 
+
+path = 'shelflist.xlsx'
+
+# get file modified date
+timestamp =  os.path.getmtime(path)
+datestamp = datetime.datetime.fromtimestamp(timestamp)
+
+# bookList = readfile.readfile(path)
+# book = bookList.getHead()
+
+taskWindow = App(title="Confirm your task")
+confirm_date = TitleBox(taskWindow, "Today is")
+Text(confirm_date, text=today, size=14)
+confirm_fileDate = TitleBox(taskWindow, "The file is uploaded on")
+Text(confirm_fileDate, text=datestamp, size=14)
+confirm_firstBook = TitleBox(taskWindow, "The first book in this list is")
+# Text(confirm_firstBook, text=book.call_number, size=14)
+# Text(confirm_firstBook, text=book.title, size=12)
+# taskWindow.set_full_screen()
+taskWindow.display()
