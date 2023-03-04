d1be14c618319c0d8b6f6fba8031886431b2b7cd
Xinyi Xu <xxu@trincoll.edu>
2023-03-03 20:22:28 -0500

    update login_UI.py

--- login_UI.py
+++ login_UI.py
@@ -4,37 +4,34 @@ from guizero import *
 import csv
 import Student
 
+filename = 'studentInfo.csv'
+loginWindow = App(title="Log in")
+Id_number = ' '
 
-def login_UI(student):
-	
-	def addStaff(id):
-		with open('studentInfo.csv', 'a+') as file:
-			file.write("\n" + str(id))
-			print('added')
+def addStaff(id):
+	with open(filename, 'a+') as file:
+		file.write("\n" + str(id))
+		print('added')
 			
-	def checkExist(id):
-		with open('studentInfo.csv', 'r') as file:
-			isExist = False
-			csvreader = csv.reader(file)
-			for row in csvreader:
-				# print(row[0])
-				# print(str(id))
-				if str(id) == row[0]:	
-					isExist = True
-			if isExist == False:
-				addStaff(id)
-		return Student.Student(id)
-
-	checkExist(student)
-
-
-def GUI_login():
-	loginWindow = App(title="Log in")
-    Text(loginWindow, text="\nWelcome!\n", size=40)
-    Text(loginWindow, text="User ID: ",align="left")
-    userIdBox = TextBox(loginWindow,text="Please enter your ID number", width=25, align="left")
-    PushButton(loginWindow, text = 'clear', command = clearTextBox, args = [userNameBox], align = "left")
-	Id_number = str(userInput.value)
+def checkExist(id):
+	with open(filename, 'r') as file:
+		isExist = False
+		csvreader = csv.reader(file)
+		for row in csvreader:
+			# print(row[0])
+			# print(str(id))
+			if str(id) == row[0]:	
+				isExist = True
+		if isExist == False:
+			addStaff(id)
+	return Student.Student(id)
+		
+def login_UI(student):
+	Text(loginWindow, text="\nWelcome!\n", size=40)
+	Text(loginWindow, text="User ID: ",align="left")
+	UserIdBox = TextBox(loginWindow,text="Please enter your ID number", width=25, align="left")
+	PushButton(loginWindow, text = 'clear', command = clearTextBox, args = [UserIdBox], align = "left")
+	Id_number = str(UserInput.value)
 	if len(Id_number) != 7:
 		loginWindow.error('!!!', 'ID number must be 7 digit!')
 	else:
@@ -42,5 +39,5 @@ def GUI_login():
 	print(Id_number)
     PushButton(loginWindow, text = 'OK', align = "bottom")
     # # PushButton(loginWindow, text = 'OK', command = GUInterface_listGenerate, args = [loginWindow], align = "bottom")
-    # # loginWindow.set_full_screen()
+    # loginWindow.set_full_screen()
     loginWindow.display()
