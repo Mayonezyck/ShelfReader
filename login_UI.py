a7d51b8a268baadca18593a84e80270895ebc028
Xinyi Xu <xxu@trincoll.edu>
2023-03-03 23:51:24 -0500

    need solve input problem

--- login_UI.py
+++ login_UI.py
@@ -30,18 +30,19 @@ def toEnter():
     UserIdBox.text_color='black'
 
 def check(id):
-	if len(id) != 7:
-		loginWindow.error('!!!', 'ID number must be 7 digit!')
+	if len(id) == 7:
+		print(len(id))
+		checkExist(id)	
 	else:
-		print('ok')
-		checkExist(id)		
+		loginWindow.error('!!!', 'ID number must be 7 digit!')	
 
 Text(loginWindow, text="\nWelcome!\n", size=40)
 Text(loginWindow, text="ID number: ",align="left")
 UserIdBox = TextBox(loginWindow, text="Please enter your ID number", width=25, align="left")
 UserIdBox.text_color = 'grey'
 UserIdBox.when_clicked = toEnter   # clear text in textbox when click on it
-id_number = str(UserIdBox.value)
+id_number = str(UserIdBox.value)  
+# value is "Please enter your ID number", use UserIdBox.append(string) to update
 arrange_box=Box(loginWindow, height="fill")
 PushButton(arrange_box, text = 'OK', command = check, args = [id_number], align = "right")
 # PushButton(loginWindow, text = 'Next', command = GUInterface_listGenerate, args = [loginWindow], align = "bottom")
