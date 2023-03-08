e034b69794d1b93f808bcbbd45de72b62d578a87
Mayonezyck <71305052+Mayonezyck@users.noreply.github.com>
2023-03-07 22:04:10 -0500

    taskui

--- task_UI.py
+++ task_UI.py
@@ -3,38 +3,34 @@
 from guizero import *
 from datetime import *
 import os
-# import readfile
-import login_UI
+import readfile
+#import login_UI
 
 class task_UI:
 
     def __init__(self) -> None:
-        self.today = None
+        self.today = date.today().strftime("%B %d, %Y") 
         self.path = 'shelflist.xlsx'
         self.taskWindow = App(title="Confirm your task")
-
-    # get today's date in textual month, day and year	
-    def getDate(self):
-        today = date.today().strftime("%B %d, %Y") 
-        return today
-
+        self.datestamp = datetime.fromtimestamp(os.path.getmtime(self.path))
+        self.bookList = readfile.readfile(self.path)
+        self.book = self.bookList.getHead()
     # get file modified date
-    timestamp = os.path.getmtime(path)
-    datestamp = datetime.fromtimestamp(timestamp)
+    
 
-    # bookList = readfile.readfile(path)
-    # book = bookList.getHead()
+    # 
 
-    def task():
-        taskWindow = App(title="Confirm your task")
-        confirm_date = TitleBox(taskWindow, "Today is")
-        Text(confirm_date, text=today, size=14)
-        confirm_fileDate = TitleBox(taskWindow, "The file is uploaded on")
-        Text(confirm_fileDate, text=datestamp, size=14)
-        confirm_firstBook = TitleBox(taskWindow, "The first book in this list is")
-        # Text(confirm_firstBook, text=book.call_number, size=14)
-        # Text(confirm_firstBook, text=book.title, size=12)
-        # taskWindow.set_full_screen()
-        taskWindow.display()
+    def task(self):
+        confirm_date = TitleBox(self.taskWindow, "Today is")
+        Text(confirm_date, text=self.today, size=14)
+        confirm_fileDate = TitleBox(self.taskWindow, "The file is uploaded on")
+        Text(confirm_fileDate, text=self.datestamp, size=14)
+        confirm_firstBook = TitleBox(self.taskWindow, "The first book in this list is")
+        Text(confirm_firstBook, text=self.book.call_number, size=14)
+        Text(confirm_firstBook, text=self.book.title, size=12)
+        #self.taskWindow.set_full_screen()
+        self.taskWindow.display()
 
 
+test = task_UI()
+test.task()
