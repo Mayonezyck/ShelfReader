f89bd2ead48abcad54f3fc19a3ba413824017f94
Xinyi Xu <xxu@trincoll.edu>
2023-03-14 16:45:17 -0400

    matrix

--- matrixgenerate.py
+++ matrixgenerate.py
@@ -9,12 +9,12 @@ class matrixgenerate:
         else:    
             self.result = []
             temp = 0
-            for i in range(len(actualBookArray)+1):
+            for i in range(len(actualBookArray)):
                 self.result.append([temp])
                 temp += 1
-            for i in range(1,len(desiredBookArray)+1):
+            for i in range(1,len(desiredBookArray)):
                 self.result[0].append(i)
-        print(self.result)
+        print(self.result[0])
     
     def A_Match_D(self,a,b,c):
         return min(a,b,c) + 0
@@ -25,40 +25,26 @@ class matrixgenerate:
     def generating(self,desiredBookArray,actualBookArray):
         d_length = len(desiredBookArray)
         a_length = len(actualBookArray)
-        row = 1
-        for i_dBook in range(d_length+1):
-            col = 1
-            for i_aBook in range(a_length+1):   
-                up_left = self.result[row-1][col-1]
-                up_right = self.result[row-1][col]
-                down_left = self.result[row][col-1]
-                print(up_left, up_right, down_left)
+        for i_aBook in range(1,a_length):
+            for i_dBook in range(1,d_length):   
+                up_left = self.result[i_aBook-1][i_dBook-1]
+                #print("lOoking for row ", i_dBook-1, "col", i_aBook)
+                up_right = self.result[i_aBook-1][i_dBook]    
+                down_left = self.result[i_aBook][i_dBook-1]
+                #print(up_left, up_right, down_left)
                 if actualBookArray[i_aBook] == desiredBookArray[i_dBook]:
                      no_action = self.A_Match_D(up_left,up_right,down_left)
-                     self.result[col].append(no_action) 
+                     self.result[i_aBook].append(no_action) 
                 else:
                      do_oneStep = self.A_notMatch_D(up_left,up_right,down_left)
-                     self.result[col].append(do_oneStep)
-                col += 1
+                     self.result[i_aBook].append(do_oneStep)
                 #print(self.result[i_dBook][i_aBook])
-                print(self.result)
-            row += 1
-        print(self.result)
-
-        #                ' ',1,2,3,4,5,6]        i_dBook = 1 -> 1
-        #  self.    ' '  [0,1,2,3,4,5,6,7]    
-        #            1   [1,0,1,"2,3,4,5,6]      i_aBook = 1 
-        #            2   [2,"1,1,2,3,4,5,5]
-        #            3   [3,"2,1,2,3,4,5,6]
-        #            5   [4,"3,2,1,2,3,4,5]
-        #            6   [5,"4,3,2,1,2,3,4]
-        #            4   [6,"5,4,3,2,1,2,3]
-        #            7   [7,"6,5,4,3,2,1,2]]
+            print(self.result[i_aBook])
 
     def getMatrix(self):
         return self.result
     
-db = [1,2,3,4,5,6]#desired book array
-ab = [1,2,3,5,6,4,7]#actual book array
+db = [None,1,2,3,4,5,6]#desired book array
+ab = [None,1,2,3,5,6,4]#actual book array
 m = matrixgenerate(db,ab)
 m.generating(db,ab)
