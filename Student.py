2d8253f3b864974f3b06a8082f261b7c3721a9d3
Xinyi Xu <xxu@trincoll.edu>
2023-03-28 17:54:12 -0400

    smth

--- Student.py
+++ Student.py
@@ -1,12 +1,17 @@
 # this class of student represents each studnet object that is unique
 
 class Student:
-	def __init__(self, ID = '00000000'):
+	def __init__(self, ID = '00000000', task_num = 0):
 		self.ID = ID
 		self.isStaff = False
+		self.task_num = task_num
 
 	def __str__(self):
 		return str(self.ID)
+	
 	def getID(self):
 		return self.ID
+	
+	def getTaskDoneNum(self):
+		return self.task_num
 
