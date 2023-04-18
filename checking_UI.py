f08e6ffa432cefdcce801aded8663b8aad451929
Mayonezyck <71305052+Mayonezyck@users.noreply.github.com>
2023-04-18 18:14:38 -0400

    set fullscreen

--- checking_UI.py
+++ checking_UI.py
@@ -57,7 +57,7 @@ class checking_UI:
             self.barcodeBox.when_clicked = toEnter
             self.ButtonBookFound = PushButton(self.shelfCheckWindow, text = 'Book Found', command = self.foundButtonPressed)
             self.ButtonSubmit = PushButton(self.shelfCheckWindow, text = 'Submit', command = self.submitButtonPressed)
-            #self.shelfCheckWindow.set_full_screen()
+            self.shelfCheckWindow.set_full_screen()
             print(self.desiredBookDic)
             self.shelfCheckWindow.display()
         
