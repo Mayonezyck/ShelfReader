162ae0a7f5d3ea8d31d75b49c4bfbdc708af3ad6
Mayonezyck <71305052+Mayonezyck@users.noreply.github.com>
2023-04-26 15:41:10 -0400

    updated end checking algorithm

--- checking_UI.py
+++ checking_UI.py
@@ -87,23 +87,24 @@ class checking_UI:
     def showNextBook(self):
         if self.currentBook is not None: 
             if self.currentBook.next_book is not None:
-                if self.currentBook.next_book.next_book is not None:
-                    #if self.currentBook.next_book.ifNeedsSkip():
-                    while int(self.currentBook.next_book.barcode) in self.desiredBookDic and self.desiredBookDic[int(self.currentBook.next_book.barcode)] is False:
-                        print(self.currentBook.title,'skiped')
-                        self.desiredBookDic[int(self.currentBook.next_book.barcode)] = True
-                        self.currentBook = self.currentBook.next_book
+                #if self.currentBook.next_book.ifNeedsSkip():
+                while int(self.currentBook.next_book.barcode) in self.desiredBookDic and self.desiredBookDic[int(self.currentBook.next_book.barcode)] is False:
+                    print(self.currentBook.title,'skiped')
+                    self.desiredBookDic[int(self.currentBook.next_book.barcode)] = True
                     self.currentBook = self.currentBook.next_book
-                    if self.currentBook.ifneedsAnounce():
-                        self.shelfCheckWindow.warn('Reshelf', 'This next book needs to be reshelved to the current position')
-                    self.BookTitle.clear()
-                    self.BookTitle.append(textwrap.fill(self.currentBook.title[:-1], width=70))
-                    self.BookCallNum.clear()
-                    self.BookCallNum.append(self.currentBook.call_number)
-                    self.BookVersion.clear()
-                    self.BookVersion.append(self.currentBook.version)
-                else:
+                self.currentBook = self.currentBook.next_book
+                if self.currentBook.ifneedsAnounce():
+                    self.shelfCheckWindow.warn('Reshelf', 'This next book needs to be reshelved to the current position')
+                self.BookTitle.clear()
+                self.BookTitle.append(textwrap.fill(self.currentBook.title[:-1], width=70))
+                self.BookCallNum.clear()
+                self.BookCallNum.append(self.currentBook.call_number)
+                self.BookVersion.clear()
+                self.BookVersion.append(self.currentBook.version)
+                if self.currentBook.next_book is None:
                     self.reachlast = True
+            else:
+                self.reachlast = True
 
     def checkIfIsTimeToReorder(self):
         #A session should only stop when: startcounting is set
